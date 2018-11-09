22fe20df0ec39f70f36d7c4c3a483c3626aa41b9
Kevin Lu <kevin.lu@nutanix.com>
2018-11-09 10:39:35 -0800

    fix link

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "google_compute_backend_service" "default" {
   protocol        = "${var.backend_protocol}"
   timeout_sec     = "${element(split(",", element(var.backend_params, count.index)), 3)}"
   backend         = ["${var.backends["${count.index}"]}"]
-  health_checks   = ["${element(google_compute_http_health_check.default.*.self_link, count.index)}"]
+  health_checks   = ["${element(google_compute_health_check.default-http.*.self_link, count.index)}"]
   security_policy = "${var.security_policy}"
   enable_cdn      = "${var.cdn}"
 }
