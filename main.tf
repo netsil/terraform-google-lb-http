3421cfccd14505ed84059e0d1c9ec6169f8f4cf4
Kevin Lu <kevin.lu@nutanix.com>
2019-11-27 18:14:12 -0500

    interpolation syntax fix

--- main.tf
+++ main.tf
@@ -15,55 +15,55 @@
  */
 
 resource "google_compute_global_forwarding_rule" "http" {
-  project    = "${var.project}"
-  count      = "${var.http_forward ? 1 : 0}"
-  name       = "${var.name}"
-  target     = "${google_compute_target_http_proxy.default[0].self_link}"
-  ip_address = "${google_compute_global_address.default.address}"
+  project    = var.project
+  count      = var.http_forward ? 1 : 0
+  name       = var.name
+  target     = google_compute_target_http_proxy.default[0].self_link
+  ip_address = google_compute_global_address.default.address
   port_range = "80"
   depends_on = ["google_compute_global_address.default"]
 }
 
 resource "google_compute_global_forwarding_rule" "https" {
-  project    = "${var.project}"
-  count      = "${var.ssl ? 1 : 0}"
+  project    = var.project
+  count      = var.ssl ? 1 : 0
   name       = "${var.name}-https"
-  target     = "${google_compute_target_https_proxy.default[0].self_link}"
-  ip_address = "${google_compute_global_address.default.address}"
+  target     = google_compute_target_https_proxy.default[0].self_link
+  ip_address = google_compute_global_address.default.address
   port_range = "443"
   depends_on = ["google_compute_global_address.default"]
 }
 
 resource "google_compute_global_address" "default" {
-  project    = "${var.project}"
+  project    = var.project
   name       = "${var.name}-address"
-  ip_version = "${var.ip_version}"
+  ip_version = var.ip_version
 }
 
 # HTTP proxy when ssl is false
 resource "google_compute_target_http_proxy" "default" {
-  project = "${var.project}"
-  count   = "${var.http_forward ? 1 : 0}"
+  project = var.project
+  count   = var.http_forward ? 1 : 0
   name    = "${var.name}-http-proxy"
   url_map = "${element(compact(concat(list(var.url_map), google_compute_url_map.default.*.self_link)), 0)}"
 }
 
 # HTTPS proxy  when ssl is true
 resource "google_compute_target_https_proxy" "default" {
-  project          = "${var.project}"
-  count            = "${var.ssl ? 1 : 0}"
+  project          = var.project
+  count            = var.ssl ? 1 : 0
   name             = "${var.name}-https-proxy"
   url_map          = "${element(compact(concat(list(var.url_map), google_compute_url_map.default.*.self_link)), 0)}"
   ssl_certificates = "${compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default.*.self_link))}"
-  ssl_policy       = "${var.ssl_policy}"
+  ssl_policy       = var.ssl_policy
 }
 
 resource "google_compute_ssl_certificate" "default" {
-  project     = "${var.project}"
+  project     = var.project
   count       = "${(var.ssl && !var.use_ssl_certificates) ? 1 : 0}"
   name_prefix = "${var.name}-certificate-"
-  private_key = "${var.private_key}"
-  certificate = "${var.certificate}"
+  private_key = var.private_key
+  certificate = var.certificate
 
   lifecycle {
     create_before_destroy = true
@@ -71,18 +71,18 @@ resource "google_compute_ssl_certificate" "default" {
 }
 
 resource "google_compute_url_map" "default" {
-  project         = "${var.project}"
-  count           = "${var.create_url_map ? 1 : 0}"
+  project         = var.project
+  count           = var.create_url_map ? 1 : 0
   name            = "${var.name}-url-map"
-  default_service = "${google_compute_backend_service.default.0.self_link}"
+  default_service = google_compute_backend_service.default.0.self_link
 }
 
 resource "google_compute_backend_service" "default" {
-  project         = "${var.project}"
-  count           = "${length(var.backend_params)}"
+  project         = var.project
+  count           = length(var.backend_params)
   name            = "${var.name}-backend-${count.index}"
   port_name       = "${element(split(",", element(var.backend_params, count.index)), 1)}"
-  protocol        = "${var.backend_protocol}"
+  protocol        = var.backend_protocol
   timeout_sec     = "${element(split(",", element(var.backend_params, count.index)), 3)}"
   dynamic "backend" {
     for_each = var.backends[count.index]
@@ -99,13 +99,13 @@ resource "google_compute_backend_service" "default" {
     }
   }
   health_checks   = ["${element(google_compute_health_check.default-tcp.*.self_link, count.index)}"]
-  security_policy = "${var.security_policy}"
-  enable_cdn      = "${var.cdn}"
+  security_policy = var.security_policy
+  enable_cdn      = var.cdn
 }
 
 resource "google_compute_health_check" "default-tcp" {
-  project      = "${var.project}"
-  count        = "${length(var.backend_params)}"
+  project      = var.project
+  count        = length(var.backend_params)
   name         = "${var.name}-backend-${count.index}"
   tcp_health_check {
     port         = "${element(split(",", element(var.backend_params, count.index)), 2)}"
@@ -119,7 +119,7 @@ resource "google_compute_firewall" "default-hc" {
   name          = "${var.name}-hc-${count.index}"
   network       = "${element(var.firewall_networks, count.index)}"
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
-  target_tags   = "${var.target_tags}"
+  target_tags   = var.target_tags
   dynamic "allow" {
     for_each = distinct(var.backend_params)
     content {
