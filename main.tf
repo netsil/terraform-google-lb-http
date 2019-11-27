889952f539dafd0fb2a2f9d19c45734068e1df74
Kevin Lu <kevin.lu@nutanix.com>
2019-11-27 17:45:55 -0500

    more fixes

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_compute_global_forwarding_rule" "http" {
   project    = "${var.project}"
   count      = "${var.http_forward ? 1 : 0}"
   name       = "${var.name}"
-  target     = "${google_compute_target_http_proxy.default.self_link}"
+  target     = "${google_compute_target_http_proxy.default[0].self_link}"
   ip_address = "${google_compute_global_address.default.address}"
   port_range = "80"
   depends_on = ["google_compute_global_address.default"]
@@ -54,7 +54,7 @@ resource "google_compute_target_https_proxy" "default" {
   count            = "${var.ssl ? 1 : 0}"
   name             = "${var.name}-https-proxy"
   url_map          = "${element(compact(concat(list(var.url_map), google_compute_url_map.default.*.self_link)), 0)}"
-  ssl_certificates = ["${compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default.*.self_link))}"]
+  ssl_certificates = "${compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default.*.self_link))}"
   ssl_policy       = "${var.ssl_policy}"
 }
 
